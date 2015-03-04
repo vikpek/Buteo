492c5e3b63d689973e6ada373765d2fe7057ff00
Viktor Pekar <vikpek@gmail.com>
2015-03-04 11:45:22 +0100

    initial unity commit

---
+++ Buteo-Unity/Buteo/Assets/BuzzardController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BuzzardController : MonoBehaviour {
+
+	Dictionary<int, GameObject> thermalColumns = new Dictionary<int,GameObject>();
+	float currentConstantForceY;
+
+	void Start()
+	{
+		InvokeRepeating("checkForThermalColumns", 0f, 1f);
+	}
+
+	void Update()
+	{
+		processThermalColumns();
+	}
+
+	void checkForThermalColumns ()
+	{
+		thermalColumns.Clear();
+		Collider[] collidingObjects = Physics.OverlapSphere(transform.position, 6f);
+		
+		foreach(Collider collider in collidingObjects)
+		{
+			if(collider.gameObject.tag == "ThermalColumn")
+			{
+				thermalColumns.Add(collider.gameObject.GetInstanceID(), collider.gameObject);
+			}
+		}
+		Debug.Log(thermalColumns.Count);
+	}
+
+	void processThermalColumns ()
+	{
+		currentConstantForceY = 0;
+		GameObject[] thermalColumnCache = new GameObject[thermalColumns.Count];
+		thermalColumns.Values.CopyTo(thermalColumnCache, 0);
+
+		for(int i = 0; i < thermalColumnCache.Length; i++)
+		{
+			currentConstantForceY += thermalColumnCache[i].GetComponent<ThermalColumnController>().ThermalImpact;
+		}
+		GetComponent<ConstantForce>().force.Set(0f, currentConstantForceY, 0f);
+		Debug.Log ("Current Y-Force: " + currentConstantForceY);
+	}
+}
---
+++ Buteo-Unity/Buteo/Assets/ThermalColumnController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThermalColumnController : MonoBehaviour {
+	[SerializeField]
+	float _thermalImpact;
+
+	public float ThermalImpact {
+		get { return _thermalImpact; }
+	}
+
+}
